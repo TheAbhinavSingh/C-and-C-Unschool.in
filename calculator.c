2cf4357a69e2fc6ddd59b8a302598a14e73d1b2b
TheAbhinavSingh <67678189+TheAbhinavSingh@users.noreply.github.com>
2020-07-05 10:48:15 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=6,b=9,c=0;
+    c=a+b;
+    printf("\n sum of a and b is %d",c);
+    c=a-b;
+    printf("\n difference of a and b is %d",c);
+    c=a*b;
+    printf("\n product of a and b is %d",c);
+    c=a/b;
+    printf("\n division of a and b is %d",c);
+    return 0;
+}
